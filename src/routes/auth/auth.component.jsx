073114bb42ed00ff9ae3b9b844c6956d6e0328d3
Sohaib Mughal <sohaib@Sohaibs-MacBook-Pro.local>
2022-09-19 19:50:54 +0500

    completed the authentication route and added everything related to it

--- src/routes/auth/auth.component.jsx
+++ src/routes/auth/auth.component.jsx
@@ -1,14 +1,16 @@
-import { getRedirectResult } from "firebase/auth";
-import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
+// import { useEffect } from "react";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
-import {
+import './auth.styles.scss'
+/* import {
   auth,
   signInWithGooglePopup,
   signInWithGoogleRedirect,
   createUserDocFromAuth,
-} from "../../utils/firebase/firebase.utils";
+} from "../../utils/firebase/firebase.utils"; */
 
-const SignIn = () => {
+const Auth = () => {
   // useEffect(()=>{
   //     const redirectResult=async ()=>{
   //         const response= await getRedirectResult(auth);
@@ -22,21 +24,17 @@ const SignIn = () => {
   //     redirectResult();
   // }, [])
 
-  const signInWithGoogle = async () => {
-    const response = await signInWithGooglePopup();
-
-    const userDocRef = await createUserDocFromAuth(response.user);
-    console.log(userDocRef);
-  };
+  
 
   return (
-    <div>
-      <h1>This is a sign in PAge</h1>
-      <button onClick={signInWithGoogle}>Sign in With Google Popup</button>
+    <div className="authentication-container">
+      {/* <h1>This is a sign in PAge</h1> */}
+      {/* <button onClick={signInWithGoogle}>Sign in With Google Popup</button> */}
       {/* <button onClick={signInWithGoogleRedirect}>Sign in With Google Redirect</button> */}
+      <SignInForm/>
       <SignUpForm />
     </div>
   );
 };
 
-export default SignIn;
+export default Auth;
